f7f87ac180ced232a7b5541ec6fea47092b38a3d
Ronald Chiang <rschaing@gmail.com>
2022-06-08 11:27:43 +1000

    fixing multiple letters bug and added random words

--- Wordle/wordle.py
+++ Wordle/wordle.py
@@ -1,26 +1,58 @@
 import requests 
+import random
+import os
 from termcolor import colored
 
-word = "flame"
+path = os.path.dirname(__file__) + "/wordlist.txt"
+f = open(path, "r").readlines()
+word = random.choice(f)[0:5]
+open(path, "r").close()
+wordindices = {}
 guess = "asdf"
 guesses = 6
+guessed = []
+
+for i in range(5):
+    if word[i] not in wordindices:
+        wordindices.update({word[i]: [i]})
+    else:
+        wordindices[word[i]].append(i)
 
 while True:
-    print(f"You have {guesses} guesses left")
-    guess = input()
+    guess = input(f"You have {guesses} guesses left: ")
     while len(guess) != 5 or type(requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + guess).json()) == dict:
-        print("That is not a valid guess. Try again.")
-        guess = input()
+        guess = input("That is not a valid guess. Try again: ")
     
-    output = []
-    for i in range(len(guess)):
-        if guess[i] == word[i]:
-            output.append(colored(guess[i], "green"))
-        elif guess[i] in word:
-            output.append(colored(guess[i], "yellow"))
+    guessindices = {}
+    for i in range(5):
+        if guess[i] not in guessindices:
+            guessindices.update({guess[i]: [i]})
         else:
+            guessindices[guess[i]].append(i)
+
+    output = []
+    for i in range(5):
+        if guess[i] not in word:
             output.append(colored(guess[i], "white"))
-    print("".join(output))
+        else:
+            a = wordindices[guess[i]]
+            b = guessindices[guess[i]]
+            matching = set(a) & set(b)
+
+            if i in matching:
+                output.append(colored(guess[i], "green"))
+            elif len(a) == len(b):
+                output.append(colored(guess[i], "yellow"))
+            else:
+                if len(a) - len(matching) == 0:
+                    output.append(colored(guess[i], "white"))
+                elif len(a) < len(b) and b.index(i) >= len(b) - len(a) - len(matching):
+                    output.append(colored(guess[i], "white"))
+                else:
+                    output.append(colored(guess[i], "yellow"))
+
+    guessed.append("".join(output))
+    print("\n".join(guessed))
 
     if guess == word:
         print("You got the word!")
@@ -29,4 +61,5 @@ while True:
     guesses -= 1
     if guesses < 1:
         print("You did not get the word :(")
+        print(f"The word was {word}")
         break
